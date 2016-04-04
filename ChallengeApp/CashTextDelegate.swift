cd92ae0e3a6d75b45ac4cf6d85de43e4ff36b55c
Magcox5 <magcox5@yahoo.com>
2016-04-04 12:54:13 -0700

    Used Ryan Collins' solution for formatting cashTextField:  works beautifully using NSNumberFormatter and testing if input entered is a number by seeing if it can be converted to an int.

--- ChallengeApp/CashTextDelegate.swift
+++ ChallengeApp/CashTextDelegate.swift
@@ -10,69 +10,45 @@ import Foundation
 import UIKit
 
 class CashTextDelegate: NSObject, UITextFieldDelegate{
-    
-    let digits = NSMutableCharacterSet.decimalDigitCharacterSet()
 
+    var numPennies = 0
     
-    //TODO:  Allow only digits convert to $ format
+    var strCents: String!
+    var strDollars: String!
+    var strMoney: String!
     
-    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    func formatMoney () -> String {
+        let numCents = numPennies % 100
+        let numDollars = numPennies / 100
         
-        // Figure out what the new text will be, if we return true
-        var newText: NSString = textField.text!
-        var newString = String(newText)
-        let charCount = newString.characters.count
-        print(newString)
-        print(charCount)
-        if cashCharactersOnly(newString) {
-            print("Only Money")
-            if charCount == 1 {
-                textField.text = "$0.0" + newString
-                return false
-            } else {
-                // more than one character, so need to make into a number first, then form the $ string
-                let cashRange = NSMakeRange (2, charCount)
-                newText = newText.stringByReplacingCharactersInRange(cashRange, withString: string)
-                newString = String(newText)
-                var tempDouble = Double(newString)
-                tempDouble = tempDouble! * 1000
-                let tempInt = Int(tempDouble!)
-                switch tempInt {
-                case 1...9 :
-                    textField.text = "$0.0" + String(tempInt)
-                case 10...99:
-                    textField.text = "$0." + String(tempInt)
-                default:
-                    let tempDollars = Int(round(tempDouble!/100))
-                    let tempPennies = Int(tempInt % 100)
-                    textField.text = "$" + String(tempDollars) + "." + String(tempPennies)
-                return false
-                }
-                
-            }
-                
+        if numCents < 10 {
+            strCents = "0\(numCents)"
+        } else {
+            strCents = "\(numCents)"
         }
-        else {
-            print("Invalid Characters")
-            return false
-        }
-        return true
-}
-    
-    
-    func cashCharactersOnly(word: String) -> Bool {
-        for character in word.unicodeScalars {
-            let charName = Character(character)
-            if !digits.longCharacterIsMember(character.value) {
-                if charName == "$" || charName == "." {
-                    return true
-                } else {
-                    return false
-                }
-                
-                            }
-        }
-        return true
+        
+        let numberFormatter = NSNumberFormatter()
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
+        strDollars = numberFormatter.stringFromNumber(numDollars)!
+        strMoney = "$\(strDollars).\(strCents)"
+        return strMoney
     }
-    
+
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        let newDigit: Int!
+        
+        //If backspace, recalculate
+        if (range.length == 1 && string.characters.count == 0) {
+            numPennies = numPennies / 10
+            textField.text = formatMoney()
+        }
+        
+        //If string is a digit, then multiply numPennies by 10 and add a new digit:
+        if Int(string) != nil {
+            newDigit = Int(string)
+            numPennies = (numPennies * 10 + newDigit)
+            textField.text = formatMoney()
+        }
+        return false
+    }
 }
